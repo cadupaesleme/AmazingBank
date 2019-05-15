85faf0e6b89e20cebca8922b0faf6a3506123c55
cadupaesleme <cadupaesleme@gmail.com>
2019-05-14 22:15:43 -0300

    repositories

---
+++ AmazingBank.Infrastructure.DataAcess/Repositories/ClientEntityFrameworkRepository.cs
@@ -0,0 +1,36 @@
+﻿using AmazingBank.DomainModel.Entities;
+using AmazingBank.DomainModel.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazingBank.Infrastructure.DataAcess.Repositories
+{
+    public class ClientEntityFrameworkRepository : IRepositoryClient
+    {
+        public void Create(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client ReadAll(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AmazingBank.Infrastructure.DataAcess/Repositories/ClientMemDbRepository.cs
@@ -0,0 +1,38 @@
+﻿using AmazingBank.DomainModel.Entities;
+using AmazingBank.DomainModel.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazingBank.Infrastructure.DataAcess.Repositories
+{
+    public class ClientMemDbRepository : IRepositoryClient
+    {
+        private static List<Client> _set = new List<Client>();
+
+        public void Create(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client ReadAll(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
